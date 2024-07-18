95e41c38a16e8a46da396a0cde962b822f89a715
giordadrian <giordadrian@gmail.com>
2024-07-17 22:44:47 -0300

    codo a codo backend team3

---
+++ src/main/java/conexion/ConexionDB.java
@@ -0,0 +1,23 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+
+    private static final String url = "jdbc:mysql://localhost:3306/movieclone";
+    private static final String user = "root";
+    private static final String password = "";
+
+    public static Connection obtenerConexion() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("No se encontró el driver JDBC", e);
+        }
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    
+}
